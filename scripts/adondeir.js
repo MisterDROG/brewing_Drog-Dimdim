bf06d31bfbefb146ab0f37c69e6edf774b45938a
DimDimShishkov <d.shishkov.russia@gmail.com>
2022-05-11 19:18:27 +0300

    feature/js brewing

--- scripts/adondeir.js
+++ scripts/adondeir.js
@@ -1,11 +1,50 @@
-const page = document.querySelector(".page");
-const header = page.querySelector(".header");
-const headerBurger = header.querySelector(".header__burger");
+const brewingSimulator = document.querySelector(".brewing__simulator");
+const brewingTables = brewingSimulator.querySelector(".brewing__tables");
+const brewingTable = brewingTables.querySelector(".brewing__table");
+const brewingBottle = brewingTables.querySelector(".brewing__bottle");
 
-/* функция открытия меню-бургера */
-const headerMenu = header.querySelector(".header__menu");
-headerBurger.addEventListener('click', function() {
-  headerBurger.classList.toggle("header__burger_active");
-  headerMenu.classList.toggle("header__menu_active");
-  page.classList.toggle("page_active");
+const templateSuslo = brewingSimulator.querySelector("#suslo").content;
+const templateAgua19L = brewingSimulator.querySelector("#agua19L").content;
+const templateAgua2L = brewingSimulator.querySelector("#agua2L").content;
+const templateHeating = brewingSimulator.querySelector("#heating").content;
+const templateWeat = brewingSimulator.querySelector("#weat").content;
+
+const button2L = document.querySelector(".brewing__button_agua2L");
+const button19L = document.querySelector(".brewing__button_agua19L");
+const buttonSuslo = document.querySelector(".brewing__button_suslo");
+const buttonheating = document.querySelector(".brewing__button_heating");
+const buttonWeat = document.querySelector(".brewing__button_weat");
+
+button2L.addEventListener("click", () => {
+ brewingBottle.prepend(templateAgua2L.cloneNode(true));
+ button2L.setAttribute("disabled", true);
 });
+
+button19L.addEventListener("click", () => {
+  brewingBottle.prepend(templateAgua19L.cloneNode(true));
+  button19L.setAttribute("disabled", true);
+ });
+
+ buttonSuslo.addEventListener("click", () => {
+  brewingBottle.prepend(templateSuslo.cloneNode(true));
+  buttonSuslo.setAttribute("disabled", true);
+ });
+
+ buttonheating.addEventListener("click", () => {
+  brewingBottle.prepend(templateHeating.cloneNode(true));
+  buttonheating.setAttribute("disabled", true);
+ });
+
+ buttonWeat.addEventListener("click", () => {
+  brewingBottle.prepend(templateWeat.cloneNode(true));
+  buttonWeat.setAttribute("disabled", true);
+  console.log(upuupu());
+ });
+
+
+
+const upuupu = () => {
+  let brewingBottleMassive = Array.prototype.slice.call(brewingBottle.childNodes);
+  brewingBottleMassive.forEach(brewingBottleMassive.clientHeight)};
+
+
