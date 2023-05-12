6218e9e90c07d555b4b845ef81156a278171c7a3
MrDrog <fireman3085@mail.ru>
2023-05-12 21:30:12 +0300

    feature: made mobile for where to go

--- src/scripts/where.js
+++ src/scripts/where.js
@@ -1,80 +1,69 @@
 import '../components/pages/where.css'
 
-function changeItem() {
-    document.getElementById('intro__text_es').style.opacity = '1';
-    document.getElementById('intro__text_rus').style.opacity = '0';
-  }// функция, которая сработает при наведении.
-  //она означает - выбрать элемент к Id у которого надо что-то изменить.
-  // когда в скобки где написано one-two добавите Id своего элемента
-  function rechangeItem() {
-    document.getElementById('intro__text_es').style.opacity = '0';
-    document.getElementById('intro__text_rus').style.opacity = '1';
-  }// тут всё также. но наобарот. протсес происходящий про отводе курсора.
+import renderFooter from './components/renderFooter'
+import renderHeader from './components/renderHeader'
 
+//render header and footer
+renderFooter()
+renderHeader()
 
-  /*
-  function changeItem() {
-    document.getElementById('intro__text_rus').style.opacity = '1';
-  }// функция, которая сработает при наведении.
-  //она означает - выбрать элемент к Id у которого надо что-то изменить.
-  // когда в скобки где написано one-two добавите Id своего элемента
-  function rechangeItem() {
-    document.getElementById('intro__text_rus').style.opacity = '0';
-  }// тут всё также. но наобарот. протсес происходящий про отводе курсора.
+// functions for language switch of the quote
+function text_es() {
+  document.getElementById('intro__text_rus').style.display = 'none';
+  document.getElementById('intro__text_en').style.display = 'none';
+  document.getElementById('intro__text_es').style.display = 'block';
+  document.getElementById('intro__language_rus').style.backgroundColor = 'grey';
+  document.getElementById('intro__language_rus').style.zIndex = '0';
+  document.getElementById('intro__language_rus').style.color = '#ffffff';
+  document.getElementById('intro__language_en').style.backgroundColor = 'grey';
+  document.getElementById('intro__language_en').style.zIndex = '0';
+  document.getElementById('intro__language_en').style.color = '#ffffff';
+  document.getElementById('intro__language_es').style.backgroundColor = '#f7f2d6';
+  document.getElementById('intro__language_es').style.zIndex = '2';
+  document.getElementById('intro__language_es').style.color = '#000';
+}
 
-  function changeItem() {
-    document.getElementById('intro__text_en').style.opacity = '1';
-  }// функция, которая сработает при наведении.
-  //она означает - выбрать элемент к Id у которого надо что-то изменить.
-  // когда в скобки где написано one-two добавите Id своего элемента
-  function rechangeItem() {
-    document.getElementById('intro__text_en').style.opacity = '0';
-  }// тут всё также. но наобарот. протсес происходящий про отводе курсора.
+function text_rus() {
+  document.getElementById('intro__text_en').style.display = 'none';
+  document.getElementById('intro__text_es').style.display = 'none';
+  document.getElementById('intro__text_rus').style.display = 'block';
+  document.getElementById('intro__language_es').style.backgroundColor = 'grey';
+  document.getElementById('intro__language_es').style.zIndex = '0';
+  document.getElementById('intro__language_es').style.color = '#ffffff';
+  document.getElementById('intro__language_en').style.backgroundColor = 'grey';
+  document.getElementById('intro__language_en').style.zIndex = '0';
+  document.getElementById('intro__language_en').style.color = '#ffffff';
+  document.getElementById('intro__language_rus').style.backgroundColor = '#f7f2d6';
+  document.getElementById('intro__language_rus').style.zIndex = '2';
+  document.getElementById('intro__language_rus').style.color = '#000';
+}
 
+function text_en() {
+  document.getElementById('intro__text_rus').style.display = 'none';
+  document.getElementById('intro__text_es').style.display = 'none';
+  document.getElementById('intro__text_en').style.display = 'block';
+  document.getElementById('intro__language_rus').style.backgroundColor = 'grey';
+  document.getElementById('intro__language_rus').style.zIndex = '0';
+  document.getElementById('intro__language_rus').style.color = '#ffffff';
+  document.getElementById('intro__language_es').style.backgroundColor = 'grey';
+  document.getElementById('intro__language_es').style.zIndex = '0';
+  document.getElementById('intro__language_es').style.color = '#ffffff';
+  document.getElementById('intro__language_en').style.backgroundColor = '#f7f2d6';
+  document.getElementById('intro__language_en').style.zIndex = '2';
+  document.getElementById('intro__language_en').style.color = '#000';
+}
 
-  */
+const textES = document.querySelector('.intro__language_es')
+textES.addEventListener('click', () => {
+    text_es()
+})
 
-  function text_es() {
-    document.getElementById('intro__text_rus').style.display = 'none';
-    document.getElementById('intro__text_en').style.display = 'none';
-    document.getElementById('intro__text_es').style.display = 'block';
-    document.getElementById('intro__language_rus').style.backgroundColor = 'grey';
-    document.getElementById('intro__language_rus').style.zIndex = '0';
-    document.getElementById('intro__language_rus').style.color = '#ffffff';
-    document.getElementById('intro__language_en').style.backgroundColor = 'grey';
-    document.getElementById('intro__language_en').style.zIndex = '0';
-    document.getElementById('intro__language_en').style.color = '#ffffff';
-    document.getElementById('intro__language_es').style.backgroundColor = 'rgb(230, 228, 222)';
-    document.getElementById('intro__language_es').style.zIndex = '2';
-    document.getElementById('intro__language_es').style.color = '#000';
-  }
+const textRU = document.querySelector('.intro__language_rus')
+textRU.addEventListener('click', () => {
+    text_rus()
+})
 
-  function text_rus() {
-    document.getElementById('intro__text_en').style.display = 'none';
-    document.getElementById('intro__text_es').style.display = 'none';
-    document.getElementById('intro__text_rus').style.display = 'block';
-    document.getElementById('intro__language_es').style.backgroundColor = 'grey';
-    document.getElementById('intro__language_es').style.zIndex = '0';
-    document.getElementById('intro__language_es').style.color = '#ffffff';
-    document.getElementById('intro__language_en').style.backgroundColor = 'grey';
-    document.getElementById('intro__language_en').style.zIndex = '0';
-    document.getElementById('intro__language_en').style.color = '#ffffff';
-    document.getElementById('intro__language_rus').style.backgroundColor = 'rgb(230, 228, 222)';
-    document.getElementById('intro__language_rus').style.zIndex = '2';
-    document.getElementById('intro__language_rus').style.color = '#000';
-  }
-
-  function text_en() {
-    document.getElementById('intro__text_rus').style.display = 'none';
-    document.getElementById('intro__text_es').style.display = 'none';
-    document.getElementById('intro__text_en').style.display = 'block';
-    document.getElementById('intro__language_rus').style.backgroundColor = 'grey';
-    document.getElementById('intro__language_rus').style.zIndex = '0';
-    document.getElementById('intro__language_rus').style.color = '#ffffff';
-    document.getElementById('intro__language_es').style.backgroundColor = 'grey';
-    document.getElementById('intro__language_es').style.zIndex = '0';
-    document.getElementById('intro__language_es').style.color = '#ffffff';
-    document.getElementById('intro__language_en').style.backgroundColor = 'rgb(230, 228, 222)';
-    document.getElementById('intro__language_en').style.zIndex = '2';
-    document.getElementById('intro__language_en').style.color = '#000';
-  }
+const textEN = document.querySelector('.intro__language_en')
+textEN.addEventListener('click', () => {
+    text_en()
+})
