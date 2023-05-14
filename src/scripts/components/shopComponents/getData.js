d0c4e686c9f3f38a4588b6f6134ac8596f899ad8
MrDrog <fireman3085@mail.ru>
2023-05-14 14:23:17 +0300

    feature: made new products grid

--- src/scripts/components/shopComponents/getData.js
+++ src/scripts/components/shopComponents/getData.js
@@ -1,9 +1,7 @@
 
 const getDataPH = () => {
-    return fetch('https://test-e79a5-default-rtdb.firebaseio.com/goods.json')
+    return fetch('https://boiz-e87ea-default-rtdb.firebaseio.com/goods.json')
     .then((response) => {
-        console.log(response)
-        console.log(response.json())
         return response.json();
     })
 }
--- src/scripts/components/shopComponents/renderGoods.js
+++ src/scripts/components/shopComponents/renderGoods.js
@@ -5,22 +5,20 @@ const renderGoods = (data) => {
 
     localStorage.setItem('goods', JSON.stringify(data))
 
-    data.forEach((item) => {
+     data.forEach((item) => {
         goodsContainer.insertAdjacentHTML('beforeend', `
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-                <div class="card" data-key="${item.id}">
-                    ${(item.sale) ? '<div class="card-sale">🔥Hot Sale🔥</div>': ''}
-                    <div class="card-img-wrapper">
-                        <span class="card-img-top"
-                            style="background-image: url('${item.img}')"></span>
-                    </div>
-                    <div class="card-body justify-content-between">
-                        <div class="card-price">${item.price} ₽</div>
-                        <h5 class="card-title">${item.title}</h5>
-                        <button class="btn btn-primary">В корзину</button>
-                    </div>
-                </div>
-            </div>`)
+        <div class="card" data-key="${item.id}">
+            ${(item.new) ? '<div class="card-sale">🔥NEW🔥</div>': ''}
+            <div class="card-img-wrapper">
+                <span class="card-img-top" style="background-image: url('${item.img}')"></span>
+            </div>
+            <div class="card-body">
+                <div class="card-price">${item.price} ₽</div>
+                <h5 class="card-title">"${item.title}"</h5>
+                <h5 class="card-subtitle">${item.subtitle}</h5>
+                <button class="btn">В корзину</button>
+            </div>
+        </div>`)
 
      })
 
